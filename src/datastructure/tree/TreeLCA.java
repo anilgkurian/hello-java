cb1a86dd4810962272af2bd083a2cb1f1e36faf5
Anil G Kurian <anilgkurian@live.com>
2016-11-28 22:41:14 +0530

    Tree LCA

--- src/datastructure/tree/TreeLCA.java
+++ src/datastructure/tree/TreeLCA.java
@@ -2,20 +2,51 @@ package datastructure.tree;
 
 public class TreeLCA {
 
-	TreeNode getLCA(TreeNode root, int n1, int n2) {
-		
-		while (root != null) {
-			if (root.val > n1 && root.val > n2)
-				root = root.left;
+	TreeNode getLCA(TreeNode node, int n1, int n2) {
 
-			else if (root.val < n1 && root.val < n2)
-				root = root.right;
+		while (node != null) {
+			if (node.val > n1 && node.val > n2)
+				node = node.left;
+
+			else if (node.val < n1 && node.val < n2)
+				node = node.right;
 
 			else
 				break;
 		}
-		System.out.println("LCA of "+n1+" and "+n2+" = "+root.val);
-		return root;
+		System.out.println("LCA of " + n1 + " & " + n2 + " = " + node.val);
+		return node;
+	}
+
+	TreeNode getLCARecursive(TreeNode node, int n1, int n2) {
+
+		if (node == null) {
+			return null;
+		}
+		if (node.val > n1 && node.val > n2)
+			return getLCARecursive(node.left, n1, n2);
+
+		if (node.val < n1 && node.val < n2)
+			return getLCARecursive(node.right, n1, n2);
+
+		System.out.println("LCA of " + n1 + " & " + n2 + " = " + node.val);
+		return node;
+	}
+
+	TreeNode getLCA(TreeNode node, int n1, int n2, int n3) {
+
+		while (node != null) {
+			if (node.val > n1 && node.val > n2 && node.val > n3)
+				node = node.left;
+
+			else if (node.val < n1 && node.val < n2 && node.val < n3)
+				node = node.right;
+
+			else
+				break;
+		}
+		System.out.println("LCA of " + n1 + ", " + n2 + " & " + n3 + " = " + node.val);
+		return node;
 	}
 
 	public static void main(String... args) {
@@ -23,6 +54,8 @@ public class TreeLCA {
 		TreeNode binaryTree = TreeSamples.getBinaryTree();
 
 		TreeLCA ob = new TreeLCA();
-		ob.getLCA(binaryTree, 10, 14);
+		ob.getLCA(binaryTree, 10, 4);
+		ob.getLCARecursive(binaryTree, 10, 4);
+		ob.getLCA(binaryTree, 10, 4, 22);
 	}
 }
