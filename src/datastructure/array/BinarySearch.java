4fcd2dc2f18960bf644e2a970053457d53db6838
Anil G Kurian <anilgkurian@live.com>
2016-12-09 12:25:30 +0530

    comments

--- src/datastructure/array/BinarySearch.java
+++ src/datastructure/array/BinarySearch.java
@@ -1,40 +1,40 @@
 package datastructure.array;
 
+/**
+ * @author Anil Kurian
+ *
+ */
 public class BinarySearch {
- // Returns index of x if it is present in arr[l..r], else
- // return -1
- int binarySearch(int arr[], int low, int high, int elem)
- {
-     if (high>=low)
-     {
-         int mid = low + (high - low)/2;
 
-         if (arr[mid] == elem)
-            return mid;
+	private static int input[] = { 2, 3, 4, 10, 40 };
 
-         if (arr[mid] > elem){
-            return binarySearch(arr, low, mid-1, elem);
-         }else{
-        	 return binarySearch(arr, mid+1, high, elem);
-         }
-     }
+	private int binarySearch(final int arr[], final int low, final int high, final int elem) {
+		if (high >= low) {
+			final int mid = low + ((high - low) / 2);
 
-     // We reach here when element is not present in array
-     return -1;
- }
+			if (arr[mid] == elem) {
+				return mid;
+			}
 
- // Driver method to test above
- public static void main(String args[])
- {
-     BinarySearch ob = new BinarySearch();
-     int arr[] = {2,3,4,10,40};
-     int n = arr.length;
-     int x = 10;
-     int result = ob.binarySearch(arr,0,n-1,x);
-     if (result == -1)
-         System.out.println("Element not present");
-     else
-         System.out.println("Element found at index "+result);
- }
+			if (arr[mid] > elem) {
+				return binarySearch(arr, low, mid - 1, elem);
+			} else {
+				return binarySearch(arr, mid + 1, high, elem);
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(final String args[]) {
+		final BinarySearch ob = new BinarySearch();
+		final int n = input.length;
+		final int x = 10;
+		final int result = ob.binarySearch(input, 0, n - 1, x);
+		if (result == -1) {
+			System.out.println("Element not present");
+		} else {
+			System.out.println("Element found at index " + result);
+		}
+	}
 }
-/* This code is contributed by Rajat Mishra */
