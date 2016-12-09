a5f56d0971fcdb2df0a5b56b60c295bf280ca042
Anil G Kurian <anilgkurian@live.com>
2016-12-09 14:05:33 +0530

    added

--- src/datastructure/set/FindPairs.java
+++ src/datastructure/set/FindPairs.java
@@ -1,25 +1,30 @@
 package datastructure.set;
+
 import java.util.HashSet;
 
+/**
+ * Find pairs of numbers which when added together gives the sum inputted.
+ *
+ * @author Anil Kurian
+ *
+ */
 public class FindPairs {
 
-	public static void main(String[] args) {
-		
-		int A[] = {1, 4, 45, 6, 10, 9,10,6};
-	    int sum = 16;
-	    
-	    HashSet<Integer> set = new HashSet<Integer>();
-	    
-	    for (int e : A) {
-	        if (set.contains(sum-e)) {
-	            System.out.println(e + "," + (sum-e));
-	            // deal with the duplicated case
-	            set.remove(sum-e);
-	            System.out.println("removed "+(sum-e)+" - "+set);
-	        } else {
-	            set.add(e);
-	            System.out.println("added "+e+" - "+set);
-	        }
-	    }
+	public static void main(final String[] args) {
+
+		final int A[] = { 11, 4, 45, 6, 10, 9, 5, 13 };
+
+		final int sum = 16;
+
+		final HashSet<Integer> set = new HashSet<Integer>();
+
+		for (final int e : A) {
+			if (set.contains(sum - e)) {
+				System.out.println(e + "," + (sum - e));
+				set.remove(sum - e);
+			} else {
+				set.add(e);
+			}
+		}
 	}
 }
