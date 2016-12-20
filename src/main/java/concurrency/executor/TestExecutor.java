31ab63821c6c7966388707691057a41a89823cf0
Anil G Kurian <anilgkurian@live.com>
2016-12-20 13:51:51 +0530

    refactor

--- src/main/java/concurrency/executor/TestExecutor.java
+++ src/main/java/concurrency/executor/TestExecutor.java
@@ -5,29 +5,41 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
+ * Using separate pools for Producers and Consumers. Using same pool can end up
+ * in consumers occupying all the threads and waiting for producers to produce,
+ * but producers will not get any threads to execute.
+ *
  * @author Anil Kurian
  *
  */
 public class TestExecutor {
 
+	private static final int PRODUCER_POOL_SIZE = 5;
+
+	private static final int CONSUMER_POOL_SIZE = 3;
+
+	private static final int NO_OF_PRODUCERS = 10;
+
+	private static final int NO_OF_CONSUMERS = 100;
+
 	public static void main(final String[] args) {
 
-		final MyPool<String> p1 = new MyPool<>(10);
+		final MyPool<String> producerPool = new MyPool<>(PRODUCER_POOL_SIZE);
 
-		final MyPool<String> p2 = new MyPool<>(3);
+		final MyPool<String> consumerPool = new MyPool<>(CONSUMER_POOL_SIZE);
 
-		final ArrayBlockingQueue<String> q = new ArrayBlockingQueue<>(30);
+		final ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(30);
 
-		for (int i = 0; i < 10; i++) {
-			final Producer p = new Producer("Producer " + i, q);
-			p1.submit(p);
+		for (int i = 0; i < NO_OF_PRODUCERS; i++) {
+			final Producer producer = new Producer("Producer " + i, queue);
+			producerPool.submit(producer);
 		}
 
-		for (int i = 0; i < 100; i++) {
-			final Consumer p = new Consumer("\t Consumer " + i, q);
-			final Future<String> submit = p2.submit(p);
+		for (int i = 0; i < NO_OF_CONSUMERS; i++) {
+			final Consumer consumer = new Consumer("\t Consumer " + i, queue);
+			final Future<String> future = consumerPool.submit(consumer);
 			try {
-				submit.get();
+				future.get();
 			} catch (InterruptedException | ExecutionException e) {
 				e.printStackTrace();
 			}
